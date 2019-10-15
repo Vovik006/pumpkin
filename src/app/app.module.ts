79f8f838f81eb294c3bf16b9955b97490f558ec7
Vova Paliichuk <vova.paliichuk@novaris.pl>
2019-10-15 15:51:31 +0200

    3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-<<<<<<< HEAD
-=======
+
 import {environment} from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 
->>>>>>> aec0aa79688773edea2a4b9e2a13d3933e363977
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ComponentsComponent } from './components/components.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-<<<<<<< HEAD
+
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-=======
+
 import { ReservationComponent } from './reservation/reservation.component';
 import { EventService } from './_services/event.service';
->>>>>>> aec0aa79688773edea2a4b9e2a13d3933e363977
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent,
     NavbarComponent,
-<<<<<<< HEAD
     DashboardComponent,
-=======
     ReservationComponent,
->>>>>>> aec0aa79688773edea2a4b9e2a13d3933e363977
   ],
   imports: [
     BrowserModule,
