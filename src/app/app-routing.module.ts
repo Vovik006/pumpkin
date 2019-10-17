468a00e5c4f3553c9cffd6d0fb38d36f79c19c79
RomanLeonovych <roman.leonovych@novaris.pl>
2019-10-17 11:49:23 +0200

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {ReservationComponent} from "./components/reservation/reservation.component";
+import {LoginComponent} from "./components/admin/login/login.component";
+import {DashboardComponent} from "./components/dashboard/dashboard.component"
 
 
 const routes: Routes = [
-  {path:'reservation', component:ReservationComponent}
+  {path:'reservation', component:ReservationComponent},
+  {path:'', component:DashboardComponent},
+  {path:'login', component:LoginComponent},
 ];
 
 @NgModule({
